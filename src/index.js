380622255cc64da89c10ed3fe40c4b06dca7d22a
MohamedJamaal <mohamed_gamal19@hotmail.com>
2021-11-25 10:18:49 +0200

    seeting up json server & submit createStream

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import redcers from './reducers';
 
 // this for devtool in browser
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(redcers, composeEnhancers(applyMiddleware()));
+const store = createStore(
+  redcers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
