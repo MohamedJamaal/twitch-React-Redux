6d2ed284087e8732a3b942c004d2f19c9a2e4aec
MohamedJamaal <mohamed_gamal19@hotmail.com>
2021-11-30 09:58:50 +0200

    Add delete-edit buttons when user logged in

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,7 +1,55 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-  return <div>Stream List</div>;
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+
+  renderAdmin(stream) {
+    // to check if user logged in or not to show delete&edit buttons
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  }
+
+  renderList() {
+    return this.props.streams.map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
+
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.descending}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+      </div>
+    );
+  }
+}
+
+const mapSTateToProps = (state) => {
+  return {
+    streams: Object.values(state.streams), // converting values inside object to array
+    currentUserId: state.auth.userId,
+  };
 };
 
-export default StreamList;
+export default connect(mapSTateToProps, { fetchStreams })(StreamList);
